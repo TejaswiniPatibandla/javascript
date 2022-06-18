60449a2e90eee04106eb22043053e70b1f082084
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-18 15:29:06 +0530

    Ass3Q3

---
+++ Filter even.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script>
+function is_even(t)
+   {
+        return(t%2==0);
+    }
+    console.log([4,5,3,62,34,21].filter(is_even));
+    //checking for example 
+    
+ console.log ([41,52,36,44,63,35,11].filter(is_even));
+</script>
+</head>
+<body>
+</body>
+</html>
