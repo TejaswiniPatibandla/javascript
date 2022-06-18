eba3cfc2e766dd9631a5e6e7366103a5c950ad64
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-18 15:26:51 +0530

    Create Sumof square.js

---
+++ Sumof square.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script>
+function ar(arr){
+var sum=0;
+for(var i=0;i<arr.length; i++){
+var k =arr[i]*arr[i];
+sum+=k
+}
+return sum;
+}console.log(ar([3,5,2,1]));
+</script>
+</head>
+<body>
+</body>
+</html>
