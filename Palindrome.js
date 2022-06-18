3e23662c8c30b42f75850f525407744731ffa2a4
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-18 18:16:10 +0530

    Ass4Q3

---
+++ Palindrome.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    <script>
+var string="malayalam";
+var len=string.length;
+for(var i=0;i<len/2;i++)
+{
+if(string[i]!==string[len-1-i]){
+    console.log ("Not a palindrome");
+}
+else
+{
+    console.log (string[i] );
+    
+}
+}</script>
+</head>
+<body>palindrome example
+</body>
+</html>
